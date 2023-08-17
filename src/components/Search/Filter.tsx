e85e4ddbbca5e033d4069b5e1a94f83a6da48886
juliandi_dandi <dandi.juliandi@nawadata.id>
2023-08-17 11:09:31 +0700

    add category, expired and apply button in filter content

--- src/components/Search/Filter.tsx
+++ src/components/Search/Filter.tsx
@@ -1,13 +1,18 @@
-import React, { useEffect, useRef } from 'react';
-import { View, Animated, Text, PanResponder, StyleSheet, Modal, TextInput, ScrollView } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { View, Animated, Text, PanResponder, StyleSheet, Modal, TextInput, ScrollView, TouchableOpacity } from 'react-native';
 import Slider from '@react-native-community/slider';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import { BLACK_COLOR, BLACK_TRANSPARENT_COLOR, BORDER_TEXT_INPUT_COLOR, DARK_GREY_COLOR, GREY_COLOR, RED_COLOR, WHITE_COLOR, WINDOW_HEIGHT, WINDOW_WIDTH } from '../../constans';
 import priceRange from '../../dummy/priceRange.json';
+import categories from '../../dummy/categories.json';
+import expired from '../../dummy/expired.json';
 
 function Filter ({modalFilterVisible, setFilterModalVisible}: any) {
 
-    const container_height = WINDOW_WIDTH * 0.2;
-    const scrollX = new Animated.Value(0);
+    const container_height = WINDOW_WIDTH * 0.576;
+
+    const [activeCategory, setActiveCategory] = useState(null);
+    const [activeExpired, setActiveExpired] = useState(null);
 
     useEffect(() => {
         if (modalFilterVisible) {
@@ -53,6 +58,57 @@ function Filter ({modalFilterVisible, setFilterModalVisible}: any) {
         })
     ).current;
 
+    const getContainerStyle = (item: any) => {
+        let arr = [];
+
+        if (item.isFirst) {
+            arr.push({ marginLeft: WINDOW_WIDTH * 0.05, marginRight: WINDOW_WIDTH * 0.025 });
+        } else if (item.isLast) {
+            arr.push({marginRight: WINDOW_WIDTH * 0.05});
+        } else {
+            arr.push({marginRight: WINDOW_WIDTH * 0.025});
+        }
+
+        if (item.isExpired) {
+            if (item.index == activeExpired) {
+                arr.push(styles.category_and_expired_container_active);
+            } else {
+                arr.push(styles.category_and_expired_container)
+            }
+        } else {
+            if (item.index == activeCategory) {
+                arr.push(styles.category_and_expired_container_active);
+            } else {
+                arr.push(styles.category_and_expired_container)
+            }
+        }
+
+        return arr;
+    }
+
+    const getExpiredAndCategoryContent = (item: any) => {
+        return (
+            <TouchableOpacity
+                key={item.index}
+                activeOpacity={1}
+                style={getContainerStyle(item)}
+                onPress={() => item.isExpired ? setActiveExpired(item.index) : setActiveCategory(item.index)}
+            >
+                {item.isExpired && (
+                    <Icon
+                        name='hourglass-half'
+                        size={WINDOW_WIDTH * 0.03}
+                        color={item.index == (item.isExpired ? activeExpired : activeCategory) ? RED_COLOR : BLACK_COLOR}
+                        style={{ marginRight: WINDOW_WIDTH * 0.015 }}
+                    />
+                )}
+                <Text style={item.index == (item.isExpired ? activeExpired : activeCategory) ? styles.category_and_expired_text_active : styles.category_and_expired_text}>
+                    {item.name}
+                </Text>
+            </TouchableOpacity>
+        )
+    }
+
     return (
         <Modal
             animationType='fade'
@@ -71,61 +127,117 @@ function Filter ({modalFilterVisible, setFilterModalVisible}: any) {
                             <Text style={styles.delete_text}>Hapus</Text>
                         </View>
                     </View>
-                    <View style={styles.price_container}>
-                        <Text style={styles.price_title}>Kisaran Harga</Text>
-                        <View style={styles.price_sub_container}>
-                            <View style={styles.price_content}>
-                                <Text style={styles.price_label}>Minimal</Text>
-                                <TextInput
-                                    style={styles.price_input}
-                                    placeholderTextColor={BLACK_COLOR}
-                                    placeholder='Rp0'
-                                    selectionColor={RED_COLOR}
-                                />
-                            </View>
-                            <View style={styles.price_content}>
-                                <Text style={styles.price_label}>Maksimal</Text>
-                                <TextInput
-                                    style={styles.price_input}
-                                    placeholderTextColor={BLACK_COLOR}
-                                    placeholder='Rp250.000'
-                                    selectionColor={RED_COLOR}
-                                />
-                            </View>
-                        </View>
-                        <Slider
-                        value={125000}
-                            style={styles.price_slider}
-                            minimumValue={0}
-                            maximumValue={250000}
-                            minimumTrackTintColor={RED_COLOR}
-                            maximumTrackTintColor={GREY_COLOR}
-                            thumbTintColor={RED_COLOR}
-                        />
-                    </View>
-                    <View>
+                    <View style={{ height: WINDOW_WIDTH }}>
                         <ScrollView
-                            horizontal={true}
-                            showsHorizontalScrollIndicator={false}
+                            showsVerticalScrollIndicator={false}
                         >
-                            {priceRange.map((item, index) => (
-                                <View
-                                    key={index}
-                                    style={[
-                                        styles.price_range_content,
-                                        index == 0
-                                        ? { marginLeft: WINDOW_WIDTH * 0.05, marginRight: WINDOW_WIDTH * 0.025 }
-                                            : index + 1 == priceRange.length
-                                            ? { marginRight: WINDOW_WIDTH * 0.05 }
-                                        : { marginRight: WINDOW_WIDTH * 0.025 }
-                                    ]}
-                                >
-                                    <Text style={styles.price_range_text}>
-                                        {item}
-                                    </Text>
+                            <View style={styles.price_container}>
+                                <Text style={styles.price_title}>Kisaran Harga</Text>
+                                <View style={styles.price_sub_container}>
+                                    <View style={styles.price_content}>
+                                        <Text style={styles.price_label}>Minimal</Text>
+                                        <TextInput
+                                            style={styles.price_input}
+                                            placeholderTextColor={BLACK_COLOR}
+                                            placeholder='Rp0'
+                                            selectionColor={RED_COLOR}
+                                        />
+                                    </View>
+                                    <View style={styles.price_content}>
+                                        <Text style={styles.price_label}>Maksimal</Text>
+                                        <TextInput
+                                            style={styles.price_input}
+                                            placeholderTextColor={BLACK_COLOR}
+                                            placeholder='Rp250.000'
+                                            selectionColor={RED_COLOR}
+                                        />
+                                    </View>
                                 </View>
-                            ))}
+                                <Slider
+                                value={125000}
+                                    style={styles.price_slider}
+                                    minimumValue={0}
+                                    maximumValue={250000}
+                                    minimumTrackTintColor={RED_COLOR}
+                                    maximumTrackTintColor={GREY_COLOR}
+                                    thumbTintColor={RED_COLOR}
+                                />
+                            </View>
+                            <View>
+                                <ScrollView
+                                    horizontal={true}
+                                    showsHorizontalScrollIndicator={false}
+                                >
+                                    {priceRange.map((item, index) => (
+                                        <View
+                                            key={index}
+                                            style={[
+                                                styles.price_range_content,
+                                                index == 0
+                                                ? { marginLeft: WINDOW_WIDTH * 0.05, marginRight: WINDOW_WIDTH * 0.025 }
+                                                    : index + 1 == priceRange.length
+                                                    ? { marginRight: WINDOW_WIDTH * 0.05 }
+                                                : { marginRight: WINDOW_WIDTH * 0.025 }
+                                            ]}
+                                        >
+                                            <Text style={styles.price_range_text}>
+                                                {item}
+                                            </Text>
+                                        </View>
+                                    ))}
+                                </ScrollView>
+                            </View>
+                            <View style={styles.category_container}>
+                                <View style={styles.category_head_container}>
+                                    <Text style={styles.category_title}>Kategori</Text>
+                                    <Text style={styles.category_button}>Lihat Semua</Text>
+                                </View>
+                                <ScrollView
+                                    horizontal={true}
+                                    showsHorizontalScrollIndicator={false}
+                                >
+                                    {categories.map((item, index) => {
+                                        let data = {
+                                            name: item,
+                                            index: index,
+                                            active: index == 2 ? true : false,
+                                            isFirst: index == 0 ? true : false,
+                                            isLast: index + 1 == categories.length ? true : false
+                                        }
+                                        return getExpiredAndCategoryContent(data);
+                                    })}
+                                </ScrollView>
+                            </View>
+                            <View style={styles.category_container}>
+                                <View style={styles.category_head_container}>
+                                    <Text style={styles.category_title}>Masa Aktif</Text>
+                                </View>
+                                <ScrollView
+                                    horizontal={true}
+                                    showsHorizontalScrollIndicator={false}
+                                >
+                                    {expired.map((item, index) => {
+                                        let data = {
+                                            name: item,
+                                            index: index,
+                                            active: index == 2 ? true : false,
+                                            isFirst: index == 0 ? true : false,
+                                            isLast: index + 1 == expired.length ? true : false,
+                                            isExpired: true,
+                                        }
+                                        return getExpiredAndCategoryContent(data);
+                                    })}
+                                </ScrollView>
+                            </View>
                         </ScrollView>
+                    </View>
+                    <View style={styles.filter_button_container}>
+                        <TouchableOpacity
+                            activeOpacity={1}
+                            style={styles.filter_sub_button_container}
+                        >
+                            <Text style={styles.filter_sub_button_text}>PASANG FILTER</Text>
+                        </TouchableOpacity>
                     </View>
                 </Animated.View>
             </View>
@@ -140,7 +252,6 @@ const styles = StyleSheet.create({
     },
     sub_container: {
         flex: 1,
-        width: WINDOW_WIDTH,
         backgroundColor: WHITE_COLOR,
         borderTopLeftRadius: WINDOW_WIDTH * 0.05,
         borderTopRightRadius: WINDOW_WIDTH * 0.05,
@@ -218,6 +329,66 @@ const styles = StyleSheet.create({
         color: BLACK_COLOR,
         fontSize: WINDOW_WIDTH * 0.035,
         fontWeight: 'bold'        
+    },
+    category_container: {
+        marginTop: WINDOW_WIDTH * 0.025
+    },
+    category_head_container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: WINDOW_WIDTH * 0.05
+    },
+    category_title: {
+        color: BLACK_COLOR,
+        fontSize: WINDOW_WIDTH * 0.045,
+        fontWeight: 'bold'
+    },
+    category_button: {
+        color: RED_COLOR,
+        fontSize: WINDOW_WIDTH * 0.035,
+        fontWeight: 'bold'
+    },
+    category_and_expired_container: {
+        backgroundColor: GREY_COLOR,
+        padding: WINDOW_WIDTH * 0.025,
+        borderRadius: WINDOW_WIDTH * 0.05,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    category_and_expired_container_active: {
+        backgroundColor: 'rgba(236, 32, 40, 0.2)',
+        padding: WINDOW_WIDTH * 0.025,
+        borderRadius: WINDOW_WIDTH * 0.05,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    category_and_expired_text: {
+        color: BLACK_COLOR,
+        fontSize: WINDOW_WIDTH * 0.045
+    },
+    category_and_expired_text_active: {
+        color: RED_COLOR,
+        fontSize: WINDOW_WIDTH * 0.045
+    },
+    filter_button_container: {
+        paddingHorizontal: WINDOW_WIDTH * 0.05,
+        backgroundColor: WHITE_COLOR,
+        height: WINDOW_WIDTH * 0.25,
+        justifyContent: 'center',
+    },
+    filter_sub_button_container: {
+        backgroundColor: RED_COLOR,
+        padding: WINDOW_WIDTH * 0.05,
+        borderRadius: WINDOW_WIDTH * 0.015,
+        alignItems: 'center'
+    },
+    filter_sub_button_text: {
+        fontWeight: 'bold',
+        color: WHITE_COLOR,
+        fontSize: WINDOW_WIDTH * 0.035
     }
 });
 
