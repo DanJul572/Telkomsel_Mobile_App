b32d65afe99f89c29fd383854fc34ba5d6f94230
juliandi_dandi <dandi.juliandi@nawadata.id>
2023-08-27 18:18:38 +0700

    close app when back button is clicked in Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,27 @@
-import * as React from 'react';
-import {SafeAreaView, ScrollView} from 'react-native';
+import React, {useEffect} from 'react';
+import {SafeAreaView, ScrollView, BackHandler} from 'react-native';
 import {Category, Info, NewContent} from '../components/Home';
+import {useNavigation} from '@react-navigation/native';
 
-function Home() {
+const Home = () => {
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        const backAction = () => {
+            if (navigation.isFocused()) {
+                BackHandler.exitApp();
+                return true;
+            }
+        };
+
+        const backHandler = BackHandler.addEventListener(
+            'hardwareBackPress',
+            backAction,
+        );
+
+        return () => backHandler.remove();
+    }, [navigation]);
+
     return (
         <SafeAreaView>
             <ScrollView showsVerticalScrollIndicator={false}>
@@ -12,6 +31,6 @@ function Home() {
             </ScrollView>
         </SafeAreaView>
     );
-}
+};
 
 export default Home;
